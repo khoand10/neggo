156241f6ac5d228999d990e8aea4c7aae6342a1f
khoand10 <khoand1011@gmail.com>
2019-10-30 01:23:13 +0700

    add button register

--- client/src/containers/Login.js
+++ client/src/containers/Login.js
@@ -26,8 +26,9 @@ class Login extends Component {
                 <Card className="text-white bg-primary py-5 d-md-down-none" style={{ width: 44 + '%' }}>
                   <CardBody className="text-center">
                     <div>
-                      <h2>Learn to code for FREE!</h2>
-                      <p>React Boilerplate with Redux, with CoreUI for UI with SCSS. Most commonly used utility libraries like Moment and Lodash are included. Axios is used to access REST APIs and Thunk Used as the Middleware.</p>
+                      <h2>Sign up</h2>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                      <button class="btn btn-primary active mt-3" type="button">Register Now!</button>
                     </div>
                   </CardBody>
                 </Card>
